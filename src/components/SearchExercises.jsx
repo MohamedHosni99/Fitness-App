ef1557592491844c851f45339811a2eb40791d67
Mohamed Hosni <mohamed.hosni9997@gmail.com>
2024-03-02 23:52:24 +0200

    Update SearchExercises.jsx

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -78,7 +78,7 @@ const SearchExercises = ({setExercises,bodyPart,setBodyPart}) => {
         </Button>
       </Box>
       <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
-        <HorizontalScrollbar data={bodyParts}  setBodyPart={setBodyPart} bodyPart={bodyPart} isBodyParts />
+        <HorizontalScrollbar data={bodyParts}  bodyParts setBodyPart={setBodyPart} bodyPart={bodyPart} isBodyParts />
       </Box>
     </Stack>
   )
